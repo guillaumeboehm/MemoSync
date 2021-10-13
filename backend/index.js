1dcfebd240e74dc1422141941cac7ecd9f2b904d
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2021-10-13 15:39:57 +0000

    backend start

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const dbName = 'memosync';
+const dbUsername = 'memosync';
+const dbPwd = 'T6ECFJvfhi6td@%@93gJ';
+const dbHost = 'localhost';
+const dbPort = '27017';
+const mongoURL = 'mongodb://'+dbUsername+':'+dbPwd+'@'+dbHost+':'+dbPort+'/';
+const collName = 'users';
+
+mongoose.connect(mongoURL);
+
+app.listen(8080, () => {
+  console.log('server listening')
+})
